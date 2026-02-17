68c7188dfec5b049f699494db28c2ed0c5d3aee8
Pietro <pietro_valocchi@yahoo.it>
2026-02-17 13:55:18 +0100

    Cattedrali

---
+++ cattedrali/app-files/data.js
@@ -0,0 +1,127 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-soissons_cathedral_interior_360x180_picardy_france_-_diliff",
+      "name": "Soissons_Cathedral_Interior_360x180,_Picardy,_France_-_Diliff",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3408,
+      "initialViewParameters": {
+        "yaw": 0.06144729338094024,
+        "pitch": -0.4405316440882725,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.002390397638444952,
+          "pitch": -0.22513866115028236,
+          "rotation": 0,
+          "target": "1-laon_cathedral_interior_360x180_picardy_france_-_diliff"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.6428864362601558,
+          "pitch": -0.014904521020874029,
+          "title": "Modello",
+          "text": "https://sketchfab.com/3d-models/tempio-greco-b0424bfa8e464593a75fa847d2be00ca"
+        }
+      ]
+    },
+    {
+      "id": "1-laon_cathedral_interior_360x180_picardy_france_-_diliff",
+      "name": "Laon_Cathedral_Interior_360x180,_Picardy,_France_-_Diliff",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3411,
+      "initialViewParameters": {
+        "yaw": 0.007123792865282752,
+        "pitch": -0.2978594639005916,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.007123792865282752,
+          "pitch": -0.2978594639005916,
+          "rotation": 0,
+          "target": "0-soissons_cathedral_interior_360x180_picardy_france_-_diliff"
+        },
+        {
+          "yaw": 0.007123792865282752,
+          "pitch": -0.2978594639005916,
+          "rotation": 0,
+          "target": "0-soissons_cathedral_interior_360x180_picardy_france_-_diliff"
+        },
+        {
+          "yaw": 0.007123792865282752,
+          "pitch": -0.2978594639005916,
+          "rotation": 0,
+          "target": "0-soissons_cathedral_interior_360x180_picardy_france_-_diliff"
+        },
+        {
+          "yaw": 0.007123792865282752,
+          "pitch": -0.2978594639005916,
+          "rotation": 0,
+          "target": "0-soissons_cathedral_interior_360x180_picardy_france_-_diliff"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.20590251781115576,
+          "pitch": 0.06400063709784476,
+          "title": "Altare",
+          "text": "Arredi"
+        }
+      ]
+    }
+  ],
+  "name": "Cattedrali",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
