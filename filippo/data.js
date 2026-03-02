c1d4ecc5f687ca3d9466a24cba87a51f06d2fc4f
Pietro <pietro_valocchi@yahoo.it>
2026-03-02 12:52:05 +0100

    filippo

---
+++ filippo/data.js
@@ -0,0 +1,71 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-photo_2026-03-02_12-16-12",
+      "name": "photo_2026-03-02_12-16-12",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 400,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9278657062694631,
+          "pitch": 0.10212826513606643,
+          "rotation": 0,
+          "target": "1-photo_2026-03-02_12-43-23"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-photo_2026-03-02_12-43-23",
+      "name": "photo_2026-03-02_12-43-23",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 400,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7401000829620017,
+          "pitch": 0.12725602416182014,
+          "rotation": 0,
+          "target": "0-photo_2026-03-02_12-16-12"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
